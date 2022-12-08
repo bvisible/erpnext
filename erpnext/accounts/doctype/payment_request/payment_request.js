03b6e0a4c3914544480ad75840a598af849a2593
bVisible <info@bvisible.ch>
2022-12-08 18:06:54 +0100

    Update payment_request.js

--- erpnext/accounts/doctype/payment_request/payment_request.js
+++ erpnext/accounts/doctype/payment_request/payment_request.js
@@ -27,7 +27,8 @@ frappe.ui.form.on("Payment Request", "onload", function(frm, dt, dn){
 frappe.ui.form.on("Payment Request", "refresh", function(frm) {
 	if(frm.doc.payment_request_type == 'Inward' && frm.doc.payment_channel !== "Phone" &&
 		!in_list(["Initiated", "Paid"], frm.doc.status) && !frm.doc.__islocal && frm.doc.docstatus==1){
-		frm.add_custom_button(__('Resend Payment Email'), function(){
+		////
+		frm.add_custom_button(__('Send by email'), function(){
 			frappe.call({
 				method: "erpnext.accounts.doctype.payment_request.payment_request.resend_payment_email",
 				args: {"docname": frm.doc.name},
