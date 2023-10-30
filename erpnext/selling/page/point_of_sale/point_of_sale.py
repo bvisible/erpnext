1e6d6f0a08e93d456911921fd1e64ab57b7f2ed4
bVisible <info@bvisible.ch>
2023-10-30 13:39:46 +0100

    Update point_of_sale.py

--- erpnext/selling/page/point_of_sale/point_of_sale.py
+++ erpnext/selling/page/point_of_sale/point_of_sale.py
@@ -203,7 +203,7 @@ def get_items(start, page_length, price_list, item_group, pos_profile, search_te
 					"currency": price.get("currency"),
 					"uom": price.uom or item.uom,
 					"batch_no": price.batch_no,
-					"item_price": item_prices.get(item_code) or {} #//// added from v14 maybe conflict
+					"item_price": item_prices.get(item_code) or {}, #//// added from v14 maybe conflict
 					"item_promo": item_promos.get(item_code) or {} #//// added from v14 maybe conflict
 				}
 			)
